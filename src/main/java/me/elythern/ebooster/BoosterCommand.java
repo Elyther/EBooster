d90a1cbe4c4c8d7eca1eb380245ac34e22c9203a
ElytherDEV <eyitlesmp@gmail.com>
2026-09-14 00:13:16 +0400

    Refactor BoosterCommand for clarity and efficiency

--- src/main/java/me/elythern/ebooster/BoosterCommand.java
+++ src/main/java/me/elythern/ebooster/BoosterCommand.java
@@ -3,38 +3,108 @@ package me.elythern.ebooster;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
-
 public class BoosterCommand implements CommandExecutor {
 
+    private EBooster plugin;
 
-private EBooster plugin;
-
-
-public BoosterCommand(EBooster plugin){
-this.plugin=plugin;
-}
+    public BoosterCommand(EBooster plugin){
+        this.plugin = plugin;
+    }
 
 
-@Override
-public boolean onCommand(
-CommandSender sender,
-Command cmd,
-String label,
-String[] args){
+    @Override
+    public boolean onCommand(CommandSender sender,
+                             Command cmd,
+                             String label,
+                             String[] args){
+
+        if(!(sender instanceof Player)){
+            return true;
+        }
+
+        Player p=(Player)sender;
 
 
-if(!(sender instanceof Player))
-return true;
+        if(args.length == 0){
+
+            new BoosterGUI(plugin).open(p);
+            return true;
+
+        }
 
 
-Player p=(Player)sender;
+        if(args[0].equalsIgnoreCase("ekle")){
 
 
-new BoosterGUI(plugin)
-.open(p);
+            if(args.length < 2){
+
+                p.sendMessage("§cKullanım: §e/booster ekle <miktar>");
+                return true;
+
+            }
 
 
-return true;
-}
+            double amount;
 
+
+            try{
+
+                amount=Double.parseDouble(args[1]);
+
+            }catch(Exception e){
+
+                p.sendMessage("§cGeçersiz miktar!");
+                return true;
+
+            }
+
+
+            if(amount <= 0){
+
+                p.sendMessage("§cMiktar 0'dan büyük olmalı!");
+                return true;
+
+            }
+
+
+            if(VaultHook.take(p,amount)){
+
+
+                double old =
+                DataManager.get(p.getUniqueId());
+
+
+                DataManager.set(
+                p.getUniqueId(),
+                old + amount);
+
+
+
+                p.sendMessage(
+                "§aBooster hesabına §e"
+                +amount+
+                "$ §ayatırıldı!");
+
+
+
+            }else{
+
+
+                p.sendMessage(
+                "§cYeterli paran yok!");
+
+            }
+
+
+            return true;
+
+        }
+
+
+
+        p.sendMessage(
+        "§cKullanım: /booster veya /booster ekle <miktar>");
+
+        return true;
+    }
 }
