1184cfa25580e77c99ae2572a1c22c0db7335e23
ElytherDEV <eyitlesmp@gmail.com>
2026-09-14 00:36:32 +0400

    Update BoosterPlaceholder.java

--- src/main/java/me/elythern/ebooster/BoosterPlaceholder.java
+++ src/main/java/me/elythern/ebooster/BoosterPlaceholder.java
@@ -1,69 +1,123 @@
 package me.elythern.ebooster;
 
-
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.entity.Player;
 
 
-public class BoosterPlaceholder 
-extends PlaceholderExpansion {
+public class BoosterPlaceholder extends PlaceholderExpansion {
 
 
-private EBooster plugin;
+    private final EBooster plugin;
 
 
-public BoosterPlaceholder(EBooster plugin){
-this.plugin=plugin;
-}
+    public BoosterPlaceholder(EBooster plugin){
 
+        this.plugin = plugin;
 
-public String getIdentifier(){
-return "ebooster";
-}
-
-
-public String getAuthor(){
-return "Elythern";
-}
-
-
-public String getVersion(){
-return "1.0";
-}
+    }
 
 
 
-public String onPlaceholderRequest(
-Player p,String id){
+    @Override
+    public String getIdentifier(){
+
+        return "ebooster";
+
+    }
 
 
-if(id.equals("balance")){
 
-return DataManager.get(
-p.getUniqueId())+"$";
+    @Override
+    public String getAuthor(){
 
-}
+        return "Elyther";
 
-
-if(id.equals("booster")){
+    }
 
 
-double balance=
-DataManager.get(
-p.getUniqueId());
+
+    @Override
+    public String getVersion(){
+
+        return "1.0";
+
+    }
 
 
-double rate =
-balance*0.0001;
+
+    @Override
+    public String onPlaceholderRequest(Player p, String id){
 
 
-return rate+"$/s";
+        double balance =
+                DataManager.get(
+                p.getUniqueId());
 
-}
 
 
-return null;
+        if(id.equals("balance")){
 
-}
+            return format(balance) + "$";
+
+        }
+
+
+
+        if(id.equals("booster")){
+
+
+            double rate =
+                    balance *
+                    plugin.getConfig()
+                    .getDouble(
+                    "booster.rate");
+
+
+
+            return "⚡ "
+                    + format(rate)
+                    + "$/s";
+
+        }
+
+
+
+        return null;
+
+    }
+
+
+
+
+    private String format(double number){
+
+
+        if(number >= 1000000000){
+
+            return String.format("%.1fB",
+            number / 1000000000);
+
+        }
+
+
+        if(number >= 1000000){
+
+            return String.format("%.1fM",
+            number / 1000000);
+
+        }
+
+
+        if(number >= 1000){
+
+            return String.format("%.1fK",
+            number / 1000);
+
+        }
+
+
+        return String.format("%.0f", number);
+
+    }
 
 }
